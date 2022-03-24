6cc1f0792d125972801628e645286c437fa67989
Jafna <syedthjafna@gmail.com>
2022-03-24 22:47:42 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthComponent } from './Auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ImageCropperModule } from 'ngx-image-cropper';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AuthComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ImageCropperModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../Auth/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit, OnDestroy {
+  private userSub!: Subscription;
+  isAuthenticated = false;
+  constructor(private authService:AuthService) { }
+ 
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user=>{
+        this.isAuthenticated = !!user;
+        console.log(!!user);
+        
+    });
+  }
+  onLogout(){
+    this.authService.logout()
+  }
+  ngOnDestroy(): void {
+   this.userSub.unsubscribe();
+  }
+
+}
